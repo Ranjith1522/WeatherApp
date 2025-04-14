89c6f8451329c6404633e56ac60112d8a8a7ce36
password <ranjithkankati2@gmail.com>
2025-04-14 22:27:03 +0530

    linked api from open source api data

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const api = "";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=hyderabad&units=metric";
+const key ="257f3851d65cf6f276b0c888f566cbed"
+
+
+var temperature = 0;
+
+
+async function getTemperature() {
+    const weatherData = await fetch(apiUrl + `&appid=${key}`);
+    var data = await weatherData.json();
+
+    console.log("DATA",data);
+    this.temperature = data.main.temp;
+    console.log("temperature",this.temperature);
+    const temp = document.querySelector('.temperature').innerHTML = this.temperature;
+}
+
+
+
+
+
+
+
+getTemperature();
