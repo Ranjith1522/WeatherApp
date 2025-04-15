443dd16ece73e7855e91a239c480c3d8be5a198e
password <ranjithkankati2@gmail.com>
2025-04-15 22:12:28 +0530

    humidity & Pressure

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const key ="257f3851d65cf6f276b0c888f566cbed"
 
 
 var temperature = 0;
+var humidity = 0;
+var pressure = 0;
 
 
 async function getTemperature() {
@@ -11,7 +13,11 @@ async function getTemperature() {
 
     console.log("DATA",data);
     this.temperature = data.main.temp;
+    this.humidity= data.main.humidity;
+    this.pressure = data.main.pressure; 
     console.log("temperature",this.temperature);
+    console.log("humidity",this.humidity);
+    console.log("pressure",this.pressure);
     const temp = document.querySelector('.temperature').innerHTML = this.temperature;
 }
 
