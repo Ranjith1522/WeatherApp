dbeee377181d4bb3b6ec2217bdf15aca5d26de7d
password <ranjithkankati2@gmail.com>
2025-04-19 13:33:04 +0530

    linked dynamic search city to api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=hyderabad&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const key ="257f3851d65cf6f276b0c888f566cbed"
 
 
@@ -17,17 +17,16 @@ async function search(){
 
 async function getTemperature(cityName) {
     console.log("City:",cityName);
-    const weatherData = await fetch(apiUrl +`${cityName}`+`&appid=${key}`);
+    const weatherData = await fetch(apiUrl +cityName+`&appid=${key}`);
     var data = await weatherData.json();
-
     console.log("DATA",data);
     this.temperature = data.main.temp;
     this.humidity= data.main.humidity;
-    this.pressure = data.main.pressure; 
-    console.log("temperature",this.temperature);
-    console.log("humidity",this.humidity);
-    console.log("pressure",this.pressure);
+    this.pressure = data.main.pressure;
+    this.city = data.name; 
+    console.log("today",this.city);
     const temp = document.querySelector('.temperature').innerHTML = this.temperature;
     const Humidity = document.querySelector('.Humidity').innerHTML = this.humidity;
-    const Pressure = document.querySelector('.Pressure').innerHTML = this.pressure;
+    const Pressure = document.querySelector('.Pressure').innerHTML = this.pressure; 
+    const citySelected = document.querySelector('.city').innerHTML=this.city;
 }
